a4931fdc9d3e892edf4d7ed9b24f6a6a8bc1ca0b
Naveen Chelo <naveenchelo@Naveens-MacBook-Air.local>
2026-01-30 18:22:24 +0530

    feat: added singleton dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,34 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
 
 module.exports = withModuleFederationPlugin({
-
   remotes: {
-    "mfe1": "http://localhost:3000/remoteEntry.js",    
+    mfe1: "http://localhost:3000/remoteEntry.js",
   },
 
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
+    "@angular/core": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+    "@angular/common": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+    "@angular/router": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+    rxjs: { singleton: true, strictVersion: true },
   },
-
 });
